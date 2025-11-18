49c9ca77db2e2136f2a0304faaf86a07f86d35c4
Vincent Potucek <vpotucek@me.com>
2025-11-18 13:14:52 +0100

    [rewrite] Add `StreamRulesRecipes`
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- guava/src/com/google/common/base/Splitter.java
+++ guava/src/com/google/common/base/Splitter.java
@@ -20,7 +20,6 @@ import static com.google.common.collect.Streams.stream;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.annotations.GwtIncompatible;
-import com.google.common.collect.Streams;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
