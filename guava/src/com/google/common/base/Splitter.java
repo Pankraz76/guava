0959db682896571442e52cf4d167ad62023b85f0
Vincent Potucek <vpotucek@me.com>
2025-11-18 13:27:06 +0100

    [rewrite] Add `StreamRulesRecipes`
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- guava/src/com/google/common/base/Splitter.java
+++ guava/src/com/google/common/base/Splitter.java
@@ -16,6 +16,7 @@ package com.google.common.base;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Streams.stream;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.annotations.GwtIncompatible;
@@ -27,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 import org.jspecify.annotations.Nullable;
 
 /**
@@ -419,8 +419,7 @@ public final class Splitter {
    * @since 28.2 (but only since 33.4.0 in the Android flavor)
    */
   public Stream<String> splitToStream(CharSequence sequence) {
-    // Can't use Streams.stream() from base
-    return StreamSupport.stream(split(sequence).spliterator(), false);
+    return stream(split(sequence));
   }
 
   /**
